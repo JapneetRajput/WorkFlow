715fd98ec200c010650d8f2001e37c15ec0b2f8a
yashviiii <yashvi.dhar@gmail.com>
2022-03-01 19:18:33 +0530

    Changes

--- app/src/main/java/com/example/workflow/dataExtract.java
+++ app/src/main/java/com/example/workflow/dataExtract.java
@@ -4,13 +4,14 @@ public class dataExtract {
 
     dataExtract(){}
 
-    private String FirstName,LastName,Email,Username;
+    private String FirstName,LastName,Email,Username,Position;
 
-    dataExtract(String FirstName,String LastName,String Username,String Email){
+    dataExtract(String FirstName,String LastName,String Username,String Email, String Position){
         this.FirstName=FirstName;
         this.LastName=LastName;
         this.Email=Email;
         this.Username=Username;
+        this.Position=Position;
     }
 
     public String getFirstName() {
@@ -25,7 +26,9 @@ public class dataExtract {
         return Email;
     }
 
-    public String getUsername() {
-        return Username;
+    public String getUsername() { return Username; }
+
+    public String getPosition() {
+        return Position;
     }
 }
